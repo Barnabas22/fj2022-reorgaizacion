840049f92645366e6658079c913672652fa5936a
Dioseth Ruiz <jaimeruizacosta@gmail.com>
2022-03-31 09:44:25 -0700

    C13

--- app/mysql.php
+++ app/mysql.php
@@ -1,3 +1,14 @@
 <?php
     //TODO: Conexion SQL
+    use \mysql;
+
+    class mysql extends \mysql {
+        protected $data = [
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'uadeo',
+            'port' => '3306';
+        ]
+    } 
 ?>
